56350c035ac3e179fb894f8ca9cf97282f4fec2d
Jyot Chirag Shah <Jshah1717@conestogac.on.ca>
2025-02-05 14:02:31 -0500

    basic comments and c++ libraries

---
+++ Receiver.cpp
@@ -0,0 +1,12 @@
+/*
+Students Name : Ashwini Gunaga and Jyot Shah
+Students Number : 8888180 and 8871717
+Assignment : Assignment-01
+Date : 02/05/2025
+File : Receiver.cpp
+*/
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <winsock2.h>
+#include <ws2tcpip.h>
---
+++ Sender.cpp
@@ -0,0 +1,7 @@
+/*
+Students Name : Ashwini Gunaga and Jyot Shah
+Students Number : 8888180 and 8871717
+Assignment : Assignment-01
+Date : 02/05/2025
+File : Sender.cpp
+*/
