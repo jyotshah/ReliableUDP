89817b375b4d4987671a9571a0529dcd76321bdd
ashwinigunaga <gunagaashwini@gmail.com>
2025-02-08 13:11:31 -0500

    Parsing arguments and socket initialization

--- Sender.cpp
+++ Sender.cpp
@@ -10,4 +10,15 @@ File : Sender.cpp
 #include <vector>
 #include<string>
 #include "Net.h"
-#include <openssl/md5.h>
+
+int main(int argc, char* argv[]) {
+    if (argc < 3) {
+        std::cerr << "Usage: " << argv[0] << " <IP> <File1> <File2> ..." << std::endl;
+        return 1;
+    }
+    if (!InitializeSockets())
+    {
+        printf("failed to initialize sockets\n");
+        return 1;
+    }
+}
