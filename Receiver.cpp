70ea27ae524cdfa36ad54892cbd06d9a98a4e74f
Jyot Chirag Shah <Jshah1717@conestogac.on.ca>
2025-02-07 22:44:05 -0500

    MINOR CHANGES

--- Receiver.cpp
+++ Receiver.cpp
@@ -19,6 +19,9 @@ const int ServerPort = 30000;
 const int ProtocolId = 0x11223344;
 const int PacketSize = 1024;
 
+string computeMD5(){
+
+}
 int main(){
     ReliableConnection connection(0x11223344, 10.0f);
     if (!connection.Start(ServerPort)) {
@@ -35,10 +38,23 @@ int main(){
         unsigned char packet[PacketSize];
         int bytesRead = connection.ReceivePacket(packet, sizeof(packet));
         if (bytesRead > 0){
-
-        } else{
+            string packetStr(reinterpret_cast<char*>(packet), bytesRead);
+            if (packetStr.rfind("EOF:",0) == 0) { 
+                expectedMD5 = packetStr.substr(4); 
+                receiving == false; 
+            }
+            else{
+                receivedData.insert(receivedData.end(), packet, packet + bytesRead);
+            }
+        } 
+    }
+        string receivedMD5 = computeMD5(receivedData);
+        if(receivedMD5 == expectedMD5){
+            ofstream outFile("received_file", ios::binary);
+            outFile.write(reinterpret_cast<const char*>(&receivedData[0]), receivedData.size());
+            cout << "File received successfully! Saved as 'received_file'.\n";
+        } else {
 
         }
-    }
 
 }
