06299a7db6310454e07171e15e3f9bc900c8e318
ashwinigunaga <gunagaashwini@gmail.com>
2025-02-08 14:06:47 -0500

    Establish connection on addr and port

--- Sender.cpp
+++ Sender.cpp
@@ -16,7 +16,7 @@ const int ProtocolId = 0x11223344;
 const int PacketSize = 1024;
 
 
-void transferFile(connection) {
+void transferFile(ReliableConnection connection, const char* file) {
     std::ifstream f(file, std::ios::binary | std::ios::ate);
     if (!f) {
         std::cerr << "Error opening file: " << file << std::endl;
@@ -46,14 +46,31 @@ int main(int argc, char* argv[]) {
         std::cerr << "Usage: " << argv[0] << " <IP> <File1> <File2> ..." << std::endl;
         return 1;
     }
+
+    int a, b, c, d;
+    Address address;
+
     if (!InitializeSockets())
     {
         printf("failed to initialize sockets\n");
         return 1;
     }
+    if (sscanf(argv[1], "%d.%d.%d.%d", &a, &b, &c, &d))
+    {
+        Address address = Address(a, b, c, d, ServerPort);
+    }
     ReliableConnection connection(ProtocolId, TimeOut);
+    Socket s;
+    s.Open(PORT);
+
+    if (!connection.Start(PORT)) {
+        printf("Failed to start connection on port \n");
+        return 1;
+    }
+    connection.Connect(address);
 
     for (int i = 2; i < argc; i++) {
-        transferFile(connection);
+        transferFile(connection, argv[i]);
     }
+    s.Close();
 }
